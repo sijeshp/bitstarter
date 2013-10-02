1a9decef46df3ee1adae90c9a47adba456eec0f2
Startup <startup@startup.(none)>
2013-10-02 23:29:14 +0400

    modified web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+app.set("view options", {layout: false});
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('index.html');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+app.use(express.static(__dirname + '/public'));
